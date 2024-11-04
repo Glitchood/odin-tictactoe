1b4a591454168f2b015eca2d4e4c5032f89a3ec0
Kriv Jhajhria <105179164+Glitchood@users.noreply.github.com>
2024-11-03 20:05:23 -0800

    🚧 Event listeners

--- script.js
+++ script.js
@@ -40,8 +40,72 @@ document.addEventListener("DOMContentLoaded", function() {
             };
         };
 
-        return { getBoard, readBoard, makeMove };
-    }
+        let winner = false;
+
+        const checkWinner = () => {
+            // check rows
+            for (let i = 0; i < 3; i++) {
+                const row = board[i];
+                let sum = 0;
+                const tokenToCheck = board[i][0].getValue();
+                if (tokenToCheck) {
+                    for (let j in row) {
+                        row[j].getValue() === tokenToCheck ? sum++ : 0;
+                    }
+                    sum === 3 ? winner = true : 0;
+                }
+            }
+
+            // check columns
+            for (let i = 0; i < 3; i++) {
+                const tokenToCheck = board[0][i].getValue();
+                if (tokenToCheck) {
+                    let sum = 0;
+                    for (let j = 0; j < 3; j++) {
+                        board[j][i].getValue() === tokenToCheck ? sum++ : 0;
+                    }
+                    sum === 3 ? winner = true : 0;
+                }
+            }
+
+            // check diagonals
+            for (let i = 0; i < 2; i++) {
+                const tokenToCheck = i === 0 ? board[0][0].getValue() : board[2][0].getValue();
+                if (tokenToCheck && i === 0) {
+                    let sum = 0;
+                    for (let j = 0; j < 3; j++) {
+                        board[j][j].getValue() === tokenToCheck ? sum++ : 0;
+                    }
+                    sum === 3 ? console.log("Winner") : 0;
+                } else if (tokenToCheck && i === 1) {
+                    let sum = 0
+                    for (let j = 2; j >= 0; j--) {
+                        board[j][2 - j].getValue() === tokenToCheck ? sum++ : 0;
+                    }
+                    sum === 3 ? winner = true : 0;
+                };
+            };
+        };
+
+        const getWinnerStatus = () => winner;
+
+        let tie = false;
+
+        const checkTie = () => {
+            let availableCells = 0;
+            for (let i in board) {
+                for (let j in board[i]) {
+                    if (!board[i][j].getValue()) { availableCells++ };
+                }
+            }
+
+            availableCells === 0 ? tie = true : console.log("More cells available");
+        }
+
+        const getTieStatus = () => tie;
+
+        return { getBoard, readBoard, makeMove, checkWinner, checkTie, getWinnerStatus, getTieStatus };
+    };
 
     const DisplayController = (function () {
 
@@ -71,14 +135,24 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log(
                 `${getCurrentPlayer().name}'s turn`
             );
+        };
+
+        const playRound = (x, y) => {
+            board.makeMove(x, y, getCurrentPlayer().token);
+
+            // check for a winner
+            board.checkWinner();
+
+
+            alternatePlayer();
+            displayNewRound();
         }
-
-
-
+        return { alternatePlayer, getCurrentPlayer, displayNewRound, playRound }
     })();
-    
-    let thisBoard = Gameboard();
-    thisBoard.makeMove(1, 2, "X");
-    thisBoard.readBoard();
 
+    const gameItems = document.querySelectorAll(".game-item");
+    gameItems.forEach((item) => item.addEventListener("click", (e) => {
+        const thisItem = e.target;
+        thisItem.textContent = "hey";
+    }))
 });
